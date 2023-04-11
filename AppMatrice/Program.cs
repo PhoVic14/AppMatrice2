74b48f6f50a57febbb84cbb2558b7c5ce4971311
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-04-11 12:32:23 +0200

    App matrice

---
+++ AppMatrice/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace AppMatrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methodes MesOutils = new methodes();
+            bool continuer = true;
+            while (continuer)
+            {
+                int choix = 0;
+                while (choix < 1 || choix > 3)
+                {
+                    Console.WriteLine("Que voulez-vous faire ?");
+                    Console.WriteLine("1. Générer un tableau aléatoire");
+                    Console.WriteLine("2. Additionner deux matrices");
+                    Console.WriteLine("3. Multiplier deux matrices");
+
+                    choix = int.Parse(Console.ReadLine());
+
+                    if (choix < 1 || choix > 3)
+                    {
+                        Console.WriteLine("Choix invalide");
+                    }
+                }
+
+                if (choix == 1)//Generer tableau aléatoire
+                {
+                    int nbLigne, nbColonne;
+                    Console.WriteLine("Saisissez le nombre de lignes du tableau :");
+                    bool nombre = int.TryParse(Console.ReadLine(), out nbLigne);
+                    Console.WriteLine("Saisissez le nombre de colonnes du tableau :");
+                    bool nombre2 = int.TryParse(Console.ReadLine(), out nbColonne);
+
+                    if (!nombre || !nombre2 || nbLigne <= 0 || nbColonne <= 0)
+                    {
+                        Console.WriteLine("Veuillez saisir des nombres positifs");
+                        continue;
+                    }
+
+                    int[,] tableauAlea;
+                    MesOutils.Generation_Tableau(nbLigne, nbColonne, out tableauAlea);
+                    Console.WriteLine("Tableau généré aléatoirement :");
+                    string stringTab;
+                    MesOutils.String_Tableau(tableauAlea, out stringTab);
+                    Console.WriteLine(stringTab);
+                }
+                else if (choix == 2)//Addition
+                {
+                    int nbLigne, nbColonne;
+                    Console.WriteLine("Saisissez le nombre de lignes des matrices :");
+                    bool nombre = int.TryParse(Console.ReadLine(), out nbLigne);
+                    Console.WriteLine("Saisissez le nombre de colonnes des matrices :");
+                    bool nombre2 = int.TryParse(Console.ReadLine(), out nbColonne);
+
+                    if (!nombre || !nombre2 || nbLigne <= 0 || nbColonne <= 0)
+                    {
+                        Console.WriteLine("Veuillez saisir des nombres positifs");
+                        continue;
+                    }
+
+                    int[,] matrice1, matrice2, matriceSum;
+                    MesOutils.Generation_Tableau(nbLigne, nbColonne, out matrice1);
+                    Console.WriteLine("Matrice 1 générée aléatoirement :");
+                    string stringMat1;
+                    MesOutils.String_Tableau(matrice1, out stringMat1);
+                    Console.WriteLine(stringMat1);
+
+                    MesOutils.Generation_Tableau(nbLigne, nbColonne, out matrice2);
+                    Console.WriteLine("Matrice 2 générée aléatoirement :");
+                    string stringMat2;
+                    MesOutils.String_Tableau(matrice2, out stringMat2);
+                    Console.WriteLine(stringMat2);
+
+                    bool addition;
+                    MesOutils.Addition_Matrice(matrice1, matrice2, out matriceSum, out addition);
+                    if (addition)
+                    {
+                        Console.WriteLine("Matrice somme :");
+                        string stringMatSum;
+                        MesOutils.String_Tableau(matriceSum, out stringMatSum);
+                        Console.WriteLine(stringMatSum);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Impossible d'additionner les matrices car elles n'ont pas la même taille.");
+                    }
+                }
+                else if (choix == 3)//Multiplication
+                {
+                    int nbLigne1, nbColonne1, nbLigne2, nbColonne2;
+                    Console.WriteLine("Saisissez le nombre de lignes de la première matrice :");
+                    bool nombre1 = int.TryParse(Console.ReadLine(), out nbLigne1);
+                    Console.WriteLine("Saisissez le nombre de colonnes de la première matrice :");
+                    bool nombre2 = int.TryParse(Console.ReadLine(), out nbColonne1);
+
+                    Console.WriteLine("Saisissez le nombre de lignes de la deuxième matrice :");
+                    bool nombre3 = int.TryParse(Console.ReadLine(), out nbLigne2);
+                    Console.WriteLine("Saisissez le nombre de colonnes de la deuxième matrice :");
+                    bool nombre4 = int.TryParse(Console.ReadLine(), out nbColonne2);
+
+                    if (!nombre1 || !nombre2 || !nombre3 || !nombre4 || nbLigne1 <= 0 || nbColonne1 <= 0 || nbLigne2 <= 0 || nbColonne2 <= 0 || nbColonne1 != nbLigne2)
+                    {
+                        Console.WriteLine("Veuillez saisir des nombres positifs et des matrices compatibles.");
+                        continue;
+                    }
+
+                    int[,] matrice1, matrice2, matriceProduct;
+                    MesOutils.Generation_Tableau(nbLigne1, nbColonne1, out matrice1);
+                    Console.WriteLine("Matrice 1 générée aléatoirement :");
+                    string stringMat1;
+                    MesOutils.String_Tableau(matrice1, out stringMat1);
+                    Console.WriteLine(stringMat1);
+
+                    MesOutils.Generation_Tableau(nbLigne2, nbColonne2, out matrice2);
+                    Console.WriteLine("Matrice 2 générée aléatoirement :");
+                    string stringMat2;
+                    MesOutils.String_Tableau(matrice2, out stringMat2);
+                    Console.WriteLine(stringMat2);
+
+                    bool multiplication;
+                    MesOutils.Multiplication_Matrice(matrice1, matrice2, out matriceProduct, out multiplication);
+                    if (multiplication)
+                    {
+                        Console.WriteLine("Matrice produit :");
+                        string stringMatProduct;
+                        MesOutils.String_Tableau(matriceProduct, out stringMatProduct);
+                        Console.WriteLine(stringMatProduct);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Impossible de multiplier les matrices.");
+                    }
+                }
+
+
+                Console.WriteLine("Voulez-vous continuer ? (O/N)");//Boucle de reprise
+                string continuerStr = Console.ReadLine();
+                continuer = (continuerStr.ToUpper() == "O");
+            }
+        }
+
+       
+    }
+}
+
---
+++ AppMatrice/methodes.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppMatrice
+{
+    public struct methodes
+    {
+        /// <summary>
+        /// Generer un tableau
+        /// </summary>
+        /// <param name="nbLigne">nombre de ligne dans la matrice</param>
+        /// <param name="nbColonne">nombre de colonne dans la matrice</param>
+        /// <param name="tableau"></param>
+        public void Generation_Tableau(int nbLigne, int nbColonne, out int[,] tableau)
+        {
+            tableau = new int[nbLigne, nbColonne];
+            Random random = new Random();
+            for (int i = 0; i < nbLigne; i++)
+            {
+                for (int j = 0; j < nbColonne; j++)
+                {
+                    tableau[i, j] = random.Next(1, 10);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Remplire la matrice
+        /// </summary>
+        /// <param name="nbLigne">nombre de colonne dans la matrice</param>
+        /// <param name="nbColonne">nombre de colonne dans la matrice</param>
+        /// <param name="matrice">matrice</param>
+        public void Remplir_Matrice(int nbLigne, int nbColonne, out int[,] matrice)
+        {
+            matrice = new int[nbLigne, nbColonne];
+            for (int i = 0; i < nbLigne; i++)
+            {
+                for (int j = 0; j < nbColonne; j++)
+                {
+                    Console.WriteLine("Saisissez l'élément à la position [{0}, {1}] :", i, j);
+                    matrice[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Additioner la matrice
+        /// </summary>
+        /// <param name="matrice1">matrice numero 1</param>
+        /// <param name="matrice2">matrice numero 2</param>
+        /// <param name="matriceSum"></param>
+        /// <param name="addition">addition de la matrice</param>
+        public void Addition_Matrice(int[,] matrice1, int[,] matrice2, out int[,] matriceSum, out bool addition)
+        {
+            int nbLignes = matrice1.GetLength(0);
+            int nbColonnes = matrice1.GetLength(1);
+            matriceSum = new int[nbLignes, nbColonnes];
+            addition = true;
+
+            for (int i = 0; i < nbLignes; i++)
+            {
+                for (int j = 0; j < nbColonnes; j++)
+                {
+                    matriceSum[i, j] = matrice1[i, j] + matrice2[i, j];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Multiplier la matrice
+        /// </summary>
+        /// <param name="matrice1">matrice numero 1</param>
+        /// <param name="matrice2">matrice numero 2</param>
+        /// <param name="matriceProduct"></param>
+        /// <param name="multiplication">multipication</param>
+        public void Multiplication_Matrice(int[,] matrice1, int[,] matrice2, out int[,] matriceProduct, out bool multiplication)
+        {
+            int nbLignes1 = matrice1.GetLength(0);
+            int nbColonnes1 = matrice1.GetLength(1);
+            int nbLignes2 = matrice2.GetLength(0);
+            int nbColonnes2 = matrice2.GetLength(1);
+            matriceProduct = new int[nbLignes1, nbColonnes2];
+            multiplication = true;
+
+            for (int i = 0; i < nbLignes1; i++)
+            {
+                for (int j = 0; j < nbColonnes2; j++)
+                {
+                    matriceProduct[i, j] = 0;
+                    for (int k = 0; k < nbColonnes1; k++)
+                    {
+                        matriceProduct[i, j] += matrice1[i, k] * matrice2[k, j];
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Mettre le tableau en string
+        /// </summary>
+        /// <param name="tableau"></param>
+        /// <param name="stringTableau"></param>
+        public void String_Tableau(int[,] tableau, out string stringTableau)
+        {
+            int nbLignes = tableau.GetLength(0);
+            int nbColonnes = tableau.GetLength(1);
+            stringTableau = "";
+
+            for (int i = 0; i < nbLignes; i++)
+            {
+                for (int j = 0; j < nbColonnes; j++)
+                {
+                    stringTableau += tableau[i, j].ToString() + "\t";
+                }
+                stringTableau += "\n";
+            }
+        }
+    }
+}
